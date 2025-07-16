30b1efc2bb234f1e2d5e32eab8277185ebefdd3e
un-owen <128820344+un-owen@users.noreply.github.com>
2025-07-16 17:22:23 +0900

    Update eva.py

--- eva.py
+++ eva.py
@@ -6,10 +6,10 @@ from main import imgEva
 model = YOLO('best.pt')
 pca = joblib.load('./pca_110.pkl')
 svm = joblib.load('./svm_c1.5_com110.pkl')
-imgEva()
 
 image_path = 'eva.jpg'
 results = model(image_path)
+imgEva(image_path)
 
 pred = results[0]
 class_index = pred.probs.top1
